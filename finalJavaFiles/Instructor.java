87d58182c6aaa4a29e89b2eed1d8e3e178645698
Judah3191n <W0509673@nscc.ca>
2025-03-27 17:10:39 -0300

    asdf

--- finalJavaFiles/Instructor.java
+++ finalJavaFiles/Instructor.java
@@ -1,6 +1,13 @@
 package finalJavaFiles;
 
 public class Instructor extends User {
+    // Instructor ID
+    private static int idCounter = 1000;
+
+    public static String generateInstructorID() {
+        return "I" + (idCounter++);
+    }
+
     // Constructor
     Instructor(String name, int age, String email) {
         super(name, age, email, "1234", "Instructor");
--- finalJavaFiles/Student.java
+++ finalJavaFiles/Student.java
@@ -1,9 +1,19 @@
 package finalJavaFiles;
 
 public class Student extends User {
+    private String major;
+    private double GPA;
+    private Boolean alumni;
+
+    private static int idCounter = 1000;
+
+    public static String generateStudentID() {
+        return "S" + (idCounter++);
+    }
+
     // Constructor
     Student(String name, int age, String email) {
-        super(name, age, email, "asdf", "Student");
+        super(name, age, email, generateStudentID(), "Student");
 
     }
 }
